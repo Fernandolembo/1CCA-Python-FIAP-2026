aaacaadd93fc578cffa6a6ee8a1e6474fa848314
Fernandolembo <fernandolembo33@hotmail.com>
2026-03-20 11:31:36 -0300

    Aula 02 variáveis e operadores

---
+++ aula02-variaveis-operadores/aula02a.py
@@ -0,0 +1,25 @@
+print ("Hello, world!")
+print (7 + 4)
+print ("7" + "4") # Concatenação de strings
+
+# Comentário de 1 linha
+'''
+Comentários de múltiplas linhas
+'''
+
+# Variáveis
+nome = "Fernando" # string
+idade = 19 # Int
+peso = 75.8 # float
+
+print (nome, idade, peso)
+print(f"oiii {nome}!!!!")
+
+# Input - simulação forms no
+nome = input("Digite o seu nome: ")
+idade = int(input("Digite sua idade: "))
+peso = input("Digite o seu peso: ")
+
+nova_idade = idade + 1
+print(nome, idade, peso)
+print(nova_idade)
---
+++ aula02-variaveis-operadores/desafios.py
@@ -0,0 +1,17 @@
+# Desafio 1
+
+nome = input ("Qual seu nome? ")
+print (f"Bem vindo, {nome}! ")
+
+#Desafio 2
+
+dia = input ("Qual o dia do seu aniversário? ")
+mes = input ("Qual o mês? ")
+ano = input ("Qual ano você nasceu? ")
+print (dia, mes, ano )
+
+# Desafio 3
+
+
+
+
