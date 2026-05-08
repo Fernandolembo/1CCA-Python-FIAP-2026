183ea45ea3cd804e5b6fbfc29f624355f5e58ff4
fernandolembo <fernandolembo33@hotmail.com>
2026-05-08 11:20:53 -0300

    Aula 5

---
+++ aula05-array-matrizes/aula05a-arrays.py
@@ -0,0 +1,20 @@
+lista_frutas = ["Maçã", "Morango", "Uva"]
+print(lista_frutas[1])
+
+lista_frutas.append("Jabuticaba")
+print(lista_frutas[-1])
+print()
+
+tamanho = len(lista_frutas)
+
+for i in range(tamanho):
+    print (lista_frutas[i])
+print()
+
+for fruta in lista_frutas:
+    print(fruta)
+print()
+
+msg = "Oi fulano!"
+for i in range (len(msg)):
+    print(msg[i])
